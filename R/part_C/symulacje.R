fdbce4a9427c58cbfce7830399585362d83157ed
Zuzia <katarzyna.streich@gmail.com>
2013-11-17 15:21:37 +0100

    poprawki w moim kodzie, plus wykresy

--- R/part_C/symulacje.R
+++ R/part_C/symulacje.R
@@ -98,10 +98,14 @@ f_dw1 <- fun_list1[[2]] ## hyhyhy dziala, tylko ze te funkcje operuja w jezyku s
 #f1 <- function(x)3*x;
 #g1 <- function(x)2*x;
 #hermit1 <- Euler_Maruyama(1, 1000, f1, g1, 1, b_m)
-hermit1 <- Euler_Maruyama(1, 1000, f_dt1, f_dw1, 1, b_m, sol)
+png(filename="hermit1.png", width=640,height=480)
 sol <- expl_solution(1,3,2,b_m, 1, 1000)
-plot(hermit1, type="l")
+hermit1 <- Euler_Maruyama(1, 1000, f_dt1, f_dw1, 1, b_m, sol)
+plot(hermit1, type="l", xlab="Czas", ylab="Wartość")
 lines(sol*2, type = "l", col = "red")
+title(main = "Hermite 1")
+legend("topleft", c("V(S)","Numerycznie V"),lty=c(1,1), lwd=c(2.5,2.5), col=c("red","black"))
+dev.off()
 
 ## Hermit 2
 #f2 <- function(x)10*x+10;
@@ -111,11 +115,16 @@ d2v2 <- function(x)8;
 fun_list2 <- ito_formula(S_f, S_g, dv2, d2v2)
 f_dt2 <- fun_list2[[1]]
 f_dw2 <- fun_list2[[2]]
+b_m <- generate_Wiener(1,1000)
+sol <- expl_solution(1,3,2,b_m, 1, 1000)
 
+png(filename="hermit2.png", width=640,height=480)
 hermit2 <- Euler_Maruyama(1, 1000, f_dt2, f_dw2, 1, b_m, sol)
-plot(hermit2, type="l")
+plot(hermit2, type="l", xlab="Czas", ylab="Wartość")
 lines(4*(sol^2)-2, type = "l", col = "red")
-
+title(main = "Hermite 2")
+legend("topleft", c("V(S)","Numerycznie V"),lty=c(1,1), lwd=c(2.5,2.5), col=c("red","black"))
+dev.off()
 ##Hermit 3
 #f3 <- function(x)36*x+360 +156*sqrt(x+6);
 #g3 <- function(x)8*x+48 + 24*sqrt(x+6);
@@ -130,9 +139,16 @@ d2v3 <- function(x)8 * (3 * (2 * x));
 fun_list3 <- ito_formula(S_f, S_g, dv3, d2v3)
 f_dt3 <- fun_list3[[1]]
 f_dw3 <- fun_list3[[2]]
+b_m <- generate_Wiener(1,1000)
+sol <- expl_solution(1,3,2,b_m, 1, 1000)
+
+png(filename="hermit3.png", width=640,height=480)
 hermit3 <- Euler_Maruyama(1, 1000, f_dt3, f_dw3, 1, b_m, sol)
-plot(hermit3, type="l")
+plot(hermit3, type="l", xlab="Czas", ylab="Wartość")
 lines((8*sol^3)-12*sol, type = "l", col = "red")
+title(main = "Hermite 3")
+legend("topleft", c("V(S)","Numerycznie V"),lty=c(1,1), lwd=c(2.5,2.5), col=c("red","black"))
+dev.off()
 
 ## Hermit 4
 ## pomocnicze liczenie pochodnych, zebym sie nie pomylila
@@ -146,9 +162,14 @@ d2v4 <- function(x)16 * (4 * (3 * x^2)) - 48 * 2;
 fun_list4 <- ito_formula(S_f, S_g, dv4, d2v4)
 f_dt4 <- fun_list4[[1]]
 f_dw4 <- fun_list4[[2]]
+
+png(filename="hermit4.png", width=640,height=480)
 hermit4 <- Euler_Maruyama(1, 1000, f_dt4, f_dw4, 1, b_m, sol)
-plot(hermit4, type="l")
+plot(hermit4, type="l", xlab="Czas", ylab="Wartość")
 lines(16*sol^4-48*sol^2+12, type = "l", col = "red")
+title(main = "Hermite 4")
+legend("topleft", c("V(S)","Numerycznie V"),lty=c(1,1), lwd=c(2.5,2.5), col=c("red","black"))
+dev.off()
 
 ## Hermit 5
 ## pomocnicze liczenie pochodnych, zebym sie nie pomylila
@@ -255,9 +276,60 @@ fun_diff <- function(vector){
   result <- vector[2:length(vector)] - vector[1:(length(vector)-1)]
   return(result)
 }
-b_m <- generate_Wiener(1, 100)
-ito_check <- fun_diff(expl_solution(1,2,1,b_m, 1, 100)) ## te hipoteze sie testuje na zwrotach
-DL.test(ito_check) ## testowanie roznicy martyngalowej? (martingale difference), UWAGA dlugo sie liczy ;)
+
+martingale_test <- function(){
+  c_res <- c()
+  k_res <- c()
+  for(i in 1:100){
+    b_m <- generate_Wiener(1, 100)
+    ito_check <- fun_diff(expl_solution(1,2,1,b_m, 1, 100)) ## te hipoteze sie testuje na zwrotach
+    dlt <- DL.test(ito_check) ## testowanie roznicy martyngalowej? (martingale difference), UWAGA dlugo sie liczy ;)
+    c_res <- c(c_res, dlt$Cp_pval)
+    k_res <- c(k_res, dlt$Kp_pval)
+  }
+  return(list(c_res, k_res))
+}
+mart_pval <- martingale_test()
+c_pvals <- mart_pval[[1]]
+k_pvals <- mart_pval[[2]]
+valid_c <- which(c_pvals>=0.05)
+valid_k <- which(k_pvals>=0.05)
+length(valid_c) ## wyszło w obu testach że 94 obserwacje pasują
+length(valid_k)
+#> mart_pval
+#[[1]]
+#[1] 0.016666667 0.570000000 0.753333333 0.873333333 0.780000000 0.356666667 0.466666667
+#[8] 0.733333333 0.370000000 0.276666667 0.776666667 0.556666667 0.080000000 0.590000000
+#[15] 0.376666667 0.840000000 0.576666667 0.513333333 0.990000000 0.453333333 0.080000000
+#[22] 0.870000000 0.056666667 0.280000000 0.390000000 0.093333333 0.240000000 0.676666667
+#[29] 0.496666667 0.106666667 0.840000000 0.710000000 0.620000000 0.233333333 0.623333333
+#[36] 0.270000000 0.680000000 0.563333333 0.976666667 0.253333333 0.730000000 0.650000000
+#[43] 0.000000000 0.283333333 0.486666667 0.990000000 0.470000000 0.293333333 0.776666667
+#[50] 0.296666667 0.636666667 0.846666667 0.053333333 0.516666667 0.486666667 0.366666667
+#[57] 0.003333333 0.120000000 0.876666667 0.926666667 0.050000000 0.753333333 0.160000000
+#[64] 0.936666667 0.296666667 0.610000000 0.100000000 0.203333333 0.773333333 0.833333333
+#[71] 0.263333333 0.260000000 0.043333333 0.626666667 0.250000000 0.343333333 0.293333333
+#[78] 0.096666667 0.423333333 0.666666667 0.096666667 0.156666667 0.706666667 0.220000000
+#[85] 0.736666667 0.290000000 0.886666667 0.373333333 0.960000000 0.850000000 0.126666667
+#[92] 0.043333333 0.050000000 0.033333333 0.413333333 0.273333333 0.596666667 0.333333333
+#[99] 0.820000000 0.930000000
+#
+#[[2]]
+#[1] 0.010000000 0.493333333 0.740000000 0.856666667 0.823333333 0.270000000 0.330000000
+#[8] 0.523333333 0.293333333 0.253333333 0.606666667 0.506666667 0.080000000 0.480000000
+#[15] 0.436666667 0.843333333 0.570000000 0.546666667 0.996666667 0.496666667 0.060000000
+#[22] 0.696666667 0.120000000 0.443333333 0.483333333 0.026666667 0.166666667 0.776666667
+#[29] 0.626666667 0.116666667 0.763333333 0.753333333 0.653333333 0.330000000 0.583333333
+#[36] 0.100000000 0.483333333 0.616666667 0.966666667 0.450000000 0.506666667 0.773333333
+#[43] 0.000000000 0.153333333 0.620000000 0.826666667 0.503333333 0.433333333 0.446666667
+#[50] 0.373333333 0.683333333 0.920000000 0.126666667 0.560000000 0.243333333 0.400000000
+#[57] 0.006666667 0.120000000 0.833333333 0.753333333 0.066666667 0.833333333 0.203333333
+#[64] 0.953333333 0.316666667 0.556666667 0.083333333 0.106666667 0.836666667 0.860000000
+#[71] 0.270000000 0.356666667 0.070000000 0.426666667 0.206666667 0.313333333 0.453333333
+#[78] 0.083333333 0.466666667 0.826666667 0.066666667 0.183333333 0.900000000 0.363333333
+#[85] 0.676666667 0.196666667 0.950000000 0.373333333 0.943333333 0.933333333 0.223333333
+#[92] 0.130000000 0.023333333 0.016666667 0.456666667 0.420000000 0.526666667 0.433333333
+#[99] 0.616666667 0.876666667
 ## sa to 2 testy
 #$Cp
 #[1] 0.0672336
